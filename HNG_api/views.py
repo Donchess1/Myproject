d48445345424cb307c438d462449b7c4129128bb
Donchess1 <Stepabod@yahoo.com>
2025-01-29 17:31:41 +0100

    Update views.py

--- HNG_api/views.py
+++ HNG_api/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 @api_view(["GET"])
 def my_Hng_api(request):
     return Response({
-        "slack_email": "stepabod@yahoo.com",
+        "email": "stepabod@yahoo.com",
         "current_datetime": datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z"),
         "github_url": "https://github.com/Donchess1/Myproject"
     }, status=status.HTTP_200_OK)
