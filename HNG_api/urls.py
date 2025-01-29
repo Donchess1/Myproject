576652de30d87b04f3792f617978686bdcadd470
Donchess1 <stepabod@yahoo.com>
2025-01-29 19:20:33 +0100

    updated view

--- HNG_api/urls.py
+++ HNG_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import my_Hng_api
 
 urlpatterns = [
-    path("", my_Hng_api, name="HNG_api"),
+    path("api/", my_Hng_api, name="HNG_api"),
 ]
--- HNG_api/views.py
+++ HNG_api/views.py
@@ -4,5 +4,5 @@ def my_Hng_api(request):
     return JsonResponse({
         "email": "stepabod@yahoo.com",
         "current_datetime": datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z"),
-        "github_url": "<https://github.com/Donchess1/Myproject>"
+        "github_url": "https://github.com/Donchess1/Myproject"
     })
