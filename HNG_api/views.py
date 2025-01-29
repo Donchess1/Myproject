f63aaf1b91ff94bc1bbe63dcc5cf1c3e69e83799
Donchess1 <stepabod@yahoo.com>
2025-01-29 19:01:52 +0100

    changed view

--- HNG_api/views.py
+++ HNG_api/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from datetime import datetime, timezone
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
-
-@api_view(["GET"])
 def my_Hng_api(request):
-    return Response({
-        "slack_email": "stepabod@yahoo.com",
+    return JsonResponse({
+        "email": "stepabod@yahoo.com",
         "current_datetime": datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z"),
-        "github_url": "https://github.com/Donchess1/Myproject.git"
-    }, status=status.HTTP_200_OK)
+        "github_url": "https://github.com/Donchess1/Myproject"
+    })
